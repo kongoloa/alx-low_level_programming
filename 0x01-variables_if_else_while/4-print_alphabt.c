46f2757d9061fe7224a5db28d19aad975135358c
kongoloa <aristoteshako@gmail.com>
2021-08-27 13:34:51 -0700

    printing alphabet in lowercase

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - main block
+ * Return: Always 0
+ */
+int main(void)
+{
+char c = 'a';
+while (c <= 'z')
+{
+if (c != 'q' && c != 'e')
+putchar(c);
+c++;
+}
+putchar('\n');
+return (0);
+}
