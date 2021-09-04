d84c920395267e65ed81552af2489742083b26e2
kongoloa <aristoteshako@gmail.com>
2021-09-04 06:58:57 -0700

    printinf an integer

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,35 +1,26 @@
 #include "main.h"
 
 /**
- * print_number - prints # using _putchar
+ * print_number - prints an integer
  * @n: integer to be printed
- * @nth: term
- * Return: void
  */
 void print_number(int n)
 {
-int copy, nth, size = 1, ones = n % 10;
+unsigned int n1;
 
-n /= 10;
-copy = n;
-if (ones < 0)
+if (n < 0)
 {
-ones *= -1, copy *= -1, n *= -1;
+n1 = -n;
 _putchar('-');
-}
-if (copy > 0)
+} else
 {
-while (copy / 10 != 0)
+n1 = n;
+}
+
+if (n1 / 10)
 {
-copy /= 10, size *= 10;
+print_number(n1 / 10);
 }
-while (size > 0)
-{
-nth = n / size;
-_putchar('0' + nth);
-n -= nth * size;
-size /= 10;
-}
-}
-_putchar('0' + ones);
+
+_putchar((n1 % 10) + '0');
 }
