3f7157cad3d4222a192d9d303769bf95e457b29d
kongoloa <aristoteshako@gmail.com>
2021-09-17 12:44:08 -0700

    multiplies two umbers

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -8,16 +8,16 @@
  */
 int main(int argc, char *argv[])
 {
-int a, b, result;
-if (argc <= 2)
+int a, b, c;
+
+if (argc != 3)
 {
-printf("Error\n");
+puts("Error");
 return (1);
 }
-/* atoi convert a string to an int */
 a = atoi(argv[1]);
 b = atoi(argv[2]);
-result = a * b;
-printf("%d\n", result);
+c = a * b;
+printf("%d\n", c);
 return (0);
 }
