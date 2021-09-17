2041165c7b2fe1723ff902034e34bc22f6ed5245
kongoloa <aristoteshako@gmail.com>
2021-09-17 12:39:43 -0700

    multiplies two numbers

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - multiplies two numbers
+ * @argc: number of arguments passed to function
+ * @argv: array of pointers to argument strings
+ * Return: 0 if no errors, else 1
+ */
+int main(int argc, char *argv[])
+{
+int a, b, result;
+if (argc <= 2)
+{
+printf("Error\n");
+return (1);
+}
+/* atoi convert a string to an int */
+a = atoi(argv[1]);
+b = atoi(argv[2]);
+result = a * b;
+printf("%d\n", result);
+return (0);
+}
