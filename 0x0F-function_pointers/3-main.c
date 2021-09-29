b7574a3b4abdff387aa49863ad5a9a453392efdc
kongoloa <aristoteshako@gmail.com>
2021-09-29 13:20:05 -0700

    3main

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,31 +1,39 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "3-calc.h"
+#include <stdlib.h>
+#include <stdio.h>
 
 /**
- * main - main function
- * @argc: number of arguments passed
- * @argv: string of arguments in array
- * Return: always 0
+ * get_op_func - function pointer that selects the correct function to perform
+ * @s: operator argument
+ * Return: function pointer corresponding to operator given
  */
 int main(int argc, char *argv[])
 {
-int a, b, c;
-int (*f)(int, int);
+int a, b;
+int (*o)(int, int);
+
 if (argc != 4)
 {
 printf("Error\n");
 exit(98);
 }
-a = atoi(argv[1]);
-b = atoi(argv[3]);
-f = get_op_func(argv[2]);
-if (f == NULL)
+if (argv[2][1] != '\0')
 {
 printf("Error\n");
 exit(99);
 }
-c = f(a, b);
-printf("%d\n", c);
+
+o = get_op_func(argv[2]);
+if (o == NULL)
+{
+printf("Error\n");
+exit(99);
+}
+
+a = atoi(argv[1]);
+b = atoi(argv[3]);
+
+printf("%d\n", o(a, b));
+
 return (0);
 }
