260515eebc1c0c4a3cc87a2b008fe34c10ad9d3a
kongoloa <aristoteshako@gmail.com>
2021-09-30 10:38:58 -0700

    sum all

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -7,15 +7,19 @@
  */
 int sum_them_all(const unsigned int n, ...)
 {
-unsigned int i;
-va_list list;
-int sum;
-
-va_start(list, n);
-for (i = 0, sum = 0; i < n; i++)
-sum += va_arg(list, int);
-
-va_end(list);
-
+va_list args;
+unsigned int i = 0;
+int sum = 0;
+if (n != 0)
+{
+va_start(args, n);
+while (i < n)
+{
+sum += va_arg(args, int);
+i++;
+}
+va_end(args);
 return (sum);
+}
+return (0);
 }
