11e278aa67f3db44fc446564766d5d6e08a4bca2
kongoloa <aristoteshako@gmail.com>
2021-10-12 13:54:18 -0700

    print (safe version)

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,33 +1,30 @@
-#include "lists.h"
 #include <stdio.h>
-#include <stdlib.h>
+#include "lists.h"
 
 /**
  * print_listint_safe - print a `listint_t` linked list including mem addresses
  * @head: list of type listint_t to print
- * Description: Go through the list only once.
+ *
  * Return: number of nodes in the list
  */
 size_t print_listint_safe(const listint_t *head)
 {
-const listint_t *current;
-size_t count;
-const listint_t *hold;
-current = head;
-if (current == NULL)
-exit(98);
-count = 0;
-while (current != NULL)
+size_t num = 0;
+long int diff;
+
+while (head)
 {
-hold = current;
-current = current->next;
-count++;
-printf("[%p] %d\n", (void *)hold, hold->n);
-if (hold < current)
+diff = head - head->next;
+num++;
+printf("[%p] %d\n", (void *)head, head->n);
+if (diff > 0)
+head = head->next;
+else
 {
-printf("-> [%p] %d\n", (void *)current, current->n);
+printf("-> [%p] %d\n", (void *)head->next, head->next->n);
 break;
 }
 }
-return (count);
+
+return (num);
 }
