5eabda28902042e9679419f8b23c917f64eb408c
kongoloa <aristoteshako@gmail.com>
2021-10-12 08:52:57 -0700

    insert

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -8,32 +8,41 @@
  * @n: value to store in node
  * Return: Address of new node or NULL if failed
  */
-listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int index, int n)
 {
-unsigned int i;
 listint_t *new;
-listint_t *temp = *head;
-new = malloc(sizeof(listint_t));
-if (!new || !head)
+listint_t *current;
+unsigned int count;
+if (head == NULL)
 return (NULL);
-new->n = n;
-new->next = NULL;
-if (idx == 0)
+current = *head;
+for (count = 1; current && count < index; count++)
 {
-new->next = *head;
-*head = new;
-return (new);
+current = current->next;
+if (current == NULL)
+return (NULL);
 }
-for (i = 0; temp && i < idx; i++)
+new = malloc(sizeof(listint_t));
+if (new == NULL)
 {
-if (i == idx - 1)
+free(new);
+return (NULL);
+}
+new->n = n;
+if (index == 0)
 {
-new->next = temp->next;
-temp->next = new;
-return (new);
+*head = new;
+new->next = current;
+}
+else if (current->next)
+{
+new->next = current->next;
+current->next = new;
 }
 else
-temp = temp->next;
+{
+new->next = NULL;
+current->next = new;
 }
-return (NULL);
+return (new);
 }
