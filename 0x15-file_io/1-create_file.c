c4233a6932c13d82207f17fa3e5388958b9d7071
kongoloa <aristoteshako@gmail.com>
2021-10-18 17:07:41 -0700

    create file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -8,20 +8,24 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-int fd, rstatus, i;
-if (filename == NULL)
+int fd, t, s = 0;
+
+if (!filename)
 return (-1);
-fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR);
-if (fd == -1)
+fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+if (fd < 0)
 return (-1);
 
 if (text_content)
 {
-for (i = 0; text_content[i] != '\0'; i++);
-rstatus = write(fd, text_content, i);
-if (rstatus == -1)
+while (text_content[s])
+s++;
+t = write(fd, text_content, s);
+if (t != s)
 return (-1);
 }
+
 close(fd);
+
 return (1);
 }
