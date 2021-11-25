e6afc484781488347f5c93fe14d3aac93af8e361
kongoloa <aristoteshako@gmail.com>
2021-11-25 15:26:44 -0800

    free list

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+
+/**
+ * free_dlistint - frees a doubly linked list
+ * @head: pointer to the list to free
+ * Return: void
+ */
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *freed;
+	for (; head != NULL;)
+	{
+		freed = head->next;
+		free(head);
+		head = freed;
+	}
+}
