000f9fcaa9d18250753ad83e7864e3e837c7752a
kongoloa <aristoteshako@gmail.com>
2021-11-25 15:44:48 -0800

    sum list

--- 0x17-doubly_linked_lists/6-sum_dlistint.c
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -7,8 +7,10 @@
  */
 int sum_dlistint(dlistint_t *head)
 {
-	int total;
-	for (total = 0; head != NULL; head = head->next)
-		total = total + head->n;
-	return (total);
+int total;
+for (total = 0; head != NULL; head = head->next)
+{
+total = total + head->n;
+}
+return (total);
 }
