86f43f0f10cd809fb8d7e2064b3f0cc78c42daf7
kongoloa <aristoteshako@gmail.com>
2021-11-25 15:43:03 -0800

    sum list

---
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,14 @@
+#include "lists.h"
+
+/**
+ * sum_dlistint - sums all of the data of a dlistint_t linked list
+ * @head: pointer to the beginning of the linked list
+ * Return: sum of the data
+ */
+int sum_dlistint(dlistint_t *head)
+{
+	int total;
+	for (total = 0; head != NULL; head = head->next)
+		total = total + head->n;
+	return (total);
+}
